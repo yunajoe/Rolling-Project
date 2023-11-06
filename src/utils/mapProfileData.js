d40a5b5508b0faea7cfda6178031f43431045cf7
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-06 10:00:19 +0900

    feat(mapProfileData.js): data포멧 필터링하는함수

--- src/utils/mapProfileData.js
+++ src/utils/mapProfileData.js
@@ -10,3 +10,15 @@ export const mapProfileData = (person) => {
     createdAt,
   };
 };
+
+export const ConvertDateFormat = (date) => {
+  const writtenDate = new Date(date);
+  const year = writtenDate.getFullYear();
+  const month = writtenDate.getMonth() + 1;
+  const day = writtenDate.getDate();
+  return {
+    year,
+    month,
+    day,
+  };
+};
