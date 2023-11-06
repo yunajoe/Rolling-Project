ef98955a9d959ccc65d37ad0652bc42ca9f8b8f7
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-06 22:06:41 +0900

    style(config.js): 변수스타일변경

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,13 +1,13 @@
-export const team = "1-6";
-export let id = 1;
+export const TEAM = "1-6";
+export let ID = 1;
 
 export const API_ENDPOINTS = {
   backgroundImages: "/background-images/",
   profileImages: "/profile-images/",
   recipients: {
-    getRecipient: `/${team}/recipients/${id}/`,
-    getRecipients: `/${team}/recipients/`,
-    getRecipientMessages: `/${team}/recipients/${id}/messages/`,
-    getRecipientReactions: `/${team}/recipients/${id}/reactions/`,
+    getRecipient: `/${TEAM}/recipients/${ID}/`,
+    getRecipients: `/${TEAM}/recipients/`,
+    getRecipientMessages: `/${TEAM}/recipients/${ID}/messages/`,
+    getRecipientReactions: `/${TEAM}/recipients/${ID}/reactions/`,
   },
 };
