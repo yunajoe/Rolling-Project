7578f660384f7190a4f6868ef608a12ba75dfd21
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-06 15:27:17 +0900

    config.js: endpoint,id,team config설정파일

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,22 +1,13 @@
 export const team = "1-6";
-export let id = "";
+export let id = 1;
 
 export const API_ENDPOINTS = {
-  baseUrl: "https://rolling-api.vercel.app",
-  backgroundImages: {
-    backgroundImages: "/background-images/",
-  },
-  profileImages: {
-    profileImages: "/profile-images/",
-  },
+  backgroundImages: "/background-images/",
+  profileImages: "/profile-images/",
   recipients: {
     getRecipient: `/${team}/recipients/${id}/`,
     getRecipients: `/${team}/recipients/`,
-    messages: {
-      getRecipientMessages: `/${team}/recipients/${id}/messages/`,
-    },
-    reactions: {
-      getRecipientReactions: `/${team}/recipients/${id}/reactions/`,
-    },
+    getRecipientMessages: `/${team}/recipients/${id}/messages/`,
+    getRecipientReactions: `/${team}/recipients/${id}/reactions/`,
   },
 };
