04baa4ef446a17fed14a2116ba7a627d213b11ab
ayden94 <144667387+ayden94@users.noreply.github.com>
2023-11-06 16:40:38 +0900

    fix(App.js) : 상태 확인을 위해 작성한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import { Outlet } from 'react-router';
 import Header from './components/Header/Header';
-import Button from './components/Button/Button';
-import binIcon from './images/binIcon.svg';
 
 const App = () => {
   return (
     <>
       <Header />
       <Outlet />
-      <Button shape="block" color="outlined" size="40">
-        <img src={binIcon} alt="" />
-        <span>안녕하세요</span>
-      </Button>
     </>
   );
 };
