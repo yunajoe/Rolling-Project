6d0dfbc13f17de3230bb029683d437243db7d740
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-06 10:04:53 +0900

    Main.js: 카드컴퍼넌트 렌더링 확인 용도로 /test 루트로 element를 받음

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App';
-import Index from './pages/Index';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Index from "./pages/Index";
+import { CardContainer } from "./components/Card/CardContainer/CardContainer";
 
 const Main = () => {
   return (
@@ -8,6 +9,7 @@ const Main = () => {
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Index />} />
+          <Route path="/test" element={<CardContainer />} />
         </Route>
       </Routes>
     </BrowserRouter>
