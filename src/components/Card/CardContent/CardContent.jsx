687db75bb9bcf93fea5525493762903e171f386e
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-06 09:07:40 +0900

    feat(CardContent.js): recepient가 나한테 쓴 글을 보여주는 component

--- src/components/Card/CardContent/CardContent.jsx
+++ src/components/Card/CardContent/CardContent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import style from "./CardContent.module.css";
 export const CardContent = ({ content }) => {
-  return <div>{content}</div>;
+  return <div className={style.container}>{content}</div>;
 };
