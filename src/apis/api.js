cc32155ca5974d623df0311f9f6a73fc33e9f797
손지은 <wiseag@naver.com>
2023-11-08 01:01:02 +0900

    test : staged 상태인 파일만 lint prettier 적용

--- src/apis/api.js
+++ src/apis/api.js
@@ -3,7 +3,9 @@ const APIpoint = "https://rolling-api.vercel.app/";
 async function requestAPI(url, option) {
   const response = await fetch(`${APIpoint}${url}`, option);
   const result = await response.json();
-  return { response, result };
+  let a;
+  a = "123";
+  return { response, result, a };
 }
 
 export default requestAPI;
