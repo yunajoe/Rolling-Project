a679830e6ee1f79252640e03382755bb831db628
ayden94 <144667387+ayden94@users.noreply.github.com>
2023-11-08 15:27:46 +0900

    feat(icon) : check, arrowDown, imojiAdd 아이콘 추가 및 폴더 구조 개선

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from './Main';
 
-import './styles/reset.css';
-import './styles/index.css';
-import './styles/fonts.css';
+import './assets/styles//reset.css';
+import './assets/styles/index.css';
+import './assets/styles/fonts.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
